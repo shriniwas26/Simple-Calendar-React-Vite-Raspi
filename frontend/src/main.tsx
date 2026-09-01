6b659032501c606e0b1d2d43b6ef7af89dfe0dde
Shriniwas Sharma <shriniwas_sharma@pm.me>
2026-09-01 20:06:44 +0200

    Revert "Switch frontend to Atkinson Hyperlegible"
    This reverts commit be8ec82f367af215b42c5dd99261f8688e6cd309.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import '@fontsource/atkinson-hyperlegible/latin-400.css';
-import '@fontsource/atkinson-hyperlegible/latin-400-italic.css';
-import '@fontsource/atkinson-hyperlegible/latin-700.css';
+import '@fontsource-variable/inter';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
